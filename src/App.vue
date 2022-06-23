3faa788b30d0a23c3a569c2e9e819498e39faf5d
qixinbo <qixinbo@gmail.com>
2022-06-23 16:28:52 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="PetDaFish logo" src="./assets/petdafish.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
